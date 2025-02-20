b2273d93c8ce8b5625f4bd444b5a602c0fd692f9
Bakhtovarkhuja <shodmonovbakha4@gmail.com>
2025-02-20 17:01:40 +0500

    lesson2.2

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '990px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      'my': '100px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+}
+
